87ddeb0653ef74dae14e1355965835a8aa6c3559
K-Dimentional Tree <kdimentionaltree@gmail.com>
2022-06-13 23:53:22 +0300

    [MINOR] Github Actions

---
+++ ton-http-api/pyTON/cli.py
@@ -0,0 +1,15 @@
+import argparse
+import uvicorn
+
+from pyTON.main import app
+
+
+# if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser('ton-http-api')
+    parser.add_argument('--host', type=str, default='localhost', help='HTTP API host')
+    parser.add_argument('--port', type=int, default=8081, help='HTTP API port')
+
+    args = parser.parse_args()
+
+    uvicorn.run(app, host=args.host, port=args.port)
